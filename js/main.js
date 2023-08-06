dbd2d6b211f63a04fade43cb8ab8b14ae02277ce
Devon <Devon.smith017@gmail.com>
2023-08-06 00:20:17 -0500

    Create function to render the gameboard array to the webpage.

--- js/main.js
+++ js/main.js
@@ -6,9 +6,22 @@ const player = (name, marker) => {
 
 const gameBoard = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
+  const getBoard = () => board;
+  return { getBoard };
 })();
 
 const gameController = (() => {
   const playerOne = ("Player One", "X");
   const playerTwo = ("Player Two", "O");
+})();
+
+const displayController = (() => {
+  const cells = document.querySelectorAll(".cell");
+
+  const updateDisplay = () => {
+    const board = gameBoard.getBoard();
+    cells.forEach((cell, index) => cell.textContent = board[index]);
+  }
+
+  updateDisplay();
 })();
