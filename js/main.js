2f79e46b4e6a52ade029ce6bfb4445dfb5350df6
Devon <Devon.smith017@gmail.com>
2023-08-06 23:35:48 -0500

    Create function to check if the game is over.

--- js/main.js
+++ js/main.js
@@ -20,15 +20,60 @@ const gameController = (() => {
   const playerOne = player("Player One", "X");
   const playerTwo = player("Player Two", "O");
   let activePlayer = playerOne;
+  let isGameOver = false;
 
   const switchActivePlayer = () => {
-    activePlayer = 
+    activePlayer =
       activePlayer === playerOne ? playerTwo : playerOne;
   }
 
   const playRound = (index) => {
     gameBoard.addMarker(index, activePlayer.getMarker());
     switchActivePlayer();
+    checkGameOver();
+  }
+
+  const getWinner = () => {
+    const board = gameBoard.getBoard();
+    const boardFull = board.every(index => index !== "");
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i <= 7; i++) {
+      const winCondition = winConditions[i];
+      let a = board[winCondition[0]];
+      let b = board[winCondition[1]];
+      let c = board[winCondition[2]];
+
+      if (a === "" || b === "" || c === "") continue;
+      if (boardFull) return isGameOver = true;
+      if (a === b && b === c) {
+        isGameOver = true;
+        return a;
+      }
+    }
+  }
+
+  const checkGameOver = () => {
+    let winner = getWinner();
+
+    if (isGameOver && winner === "X") {
+      console.log(playerOne.getName() + " Wins");
+    }
+    else if (isGameOver && winner === "O") {
+      console.log(playerTwo.getName() + " Wins");
+    }
+    else if (isGameOver) {
+      console.log("It's a tie!!");
+    }
   }
 
   return { playRound };
