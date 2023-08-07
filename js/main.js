8b6dcff0e54b091cc93e09dcdd7270eb387c01a4
Devon <Devon.smith017@gmail.com>
2023-08-06 20:36:11 -0500

    Create function to render markers to the game board.

--- js/main.js
+++ js/main.js
@@ -17,17 +17,33 @@ const gameBoard = (() => {
 })();
 
 const gameController = (() => {
-  const playerOne = ("Player One", "X");
-  const playerTwo = ("Player Two", "O");
+  const playerOne = player("Player One", "X");
+  const playerTwo = player("Player Two", "O");
+  const activePlayer = playerOne;
+
+  const playRound = (index) => {
+    gameBoard.addMarker(index, activePlayer.getMarker());
+  }
+
+  return { playRound };
 })();
 
 const displayController = (() => {
+  const boardEl = document.querySelector(".board");
   const cells = document.querySelectorAll(".cell");
 
   const updateDisplay = () => {
     const board = gameBoard.getBoard();
     cells.forEach((cell, index) => cell.textContent = board[index]);
   }
+
+  const renderMarker = (e) => {
+    const index = e.target.dataset.id;
+    gameController.playRound(index);
+    updateDisplay();
+  }
+
+  boardEl.addEventListener("click", renderMarker);
 
   updateDisplay();
 })();
